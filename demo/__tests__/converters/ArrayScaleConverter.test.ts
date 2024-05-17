42ac8e15413d7b2fc1b60be13ca833b7460c29fa
Raja K <ksvraja@gmail.com>
2024-05-18 00:40:28 +0530

    Sample test case added

---
+++ demo/__tests__/converters/ArrayScaleConverter.test.ts
@@ -0,0 +1,13 @@
+import { ArrayScaleConverter } from "../../../src/palmyra/chart/chartjs/converters/base/ArrayScaleConverter";
+
+test('sdf', () => {
+  const c = ArrayScaleConverter({});
+  const o = c([
+    { name: 'n23', value: 23 },
+    { name: 'n24', value: 24 },
+  ])
+
+  const data = o.datasets[0].data;  
+  expect(data).toEqual(expect.arrayContaining([23, 24]))
+  expect(o.labels).toEqual(expect.arrayContaining(['n23', 'n24']))
+});
--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,10 @@ export default {
     // process `*.tsx` files with `ts-jest`        
         "^.+\\.tsx?$": "ts-jest"
     },
-    rootDir: 'src',
+    rootDir: 'demo',
     moduleNameMapper: {
         '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',        
         "\\.(css|less)$": "<rootDir>/test/__mocks__/styleMock.js",
-        '^@app/(.*)$': '<rootDir>/$1'
+        '^@app/(.*)$': 'src/$1'
     }
 }
